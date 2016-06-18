7e68d1ac81f3d8c2371c96bc30aca9f4901d5985
Amy McCarthy <amy.mccarthy@ostmodern.co.uk>
2016-06-18 20:26:51 +0100

    display random carousel gif from giphy api

--- index.ios.js
+++ index.ios.js
@@ -1,86 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
+var React = require('react');
+var ReactNative = require('react-native');
+var {
   AppRegistry,
   Animated,
+  Navigator,
+  NavigatorIOS,
   PanResponder,
   Image,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View
-} from 'react-native';
+} = ReactNative;
 
-class endlesss extends Component {
-  componentWillMount() {
-    this._panResponder = PanResponder.create({
-      onMoveShouldSetResponderCapture: () => true,
-      onMoveShouldSetPanResponderCapture: () => true,
+var Intro = require('./Intro');
 
-      onPanResponderGrant: (e, gestureState) => {
-        //console.log('e', e);
-
-      },
-
-      onPanResponderMove: Animated.event([
-        // console.log('moving');
-      ]),
-
-      onPanResponderRelease: (e, {vx, vy}) => {
-        console.log('release');
-        fetch("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=carousel", {method: "GET"})
-        .then((response) => response.json())
-        .then((responseData) => {
-          console.log('response', responseData.data.fixed_height_downsampled_url, responseData);
-          this.imageUrl = responseData.data.fixed_height_downsampled_url;
-
-        })
-        .done();
-      }
-    });
-  }
-
-  render() {
+var endlesss = React.createClass({
+  render: function() {
     return (
-      <View style={styles.container}>
-        <Animated.View {...this._panResponder.panHandlers}>
-          <Image source={{ uri: this.imageUrl }}>
-            <Text style={styles.welcome}>
-              Welcome to the rest of your life!
-            </Text>
-            <Text style={styles.instructions}>
-              Tap to see whats in store
-            </Text>
-          </Image>
-        </Animated.View>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Endlesss',
+          component: Intro,
+        }}
+      />
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'white',
   },
 });
 
 AppRegistry.registerComponent('endlesss', () => endlesss);
 
-
+module.exports = endlesss;
